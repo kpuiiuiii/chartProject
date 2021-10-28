608b8135afe9f7d8b7c28ed08bce024652d2a110
Maksym Melnyk <43916250+kpuiiuiii@users.noreply.github.com>
2021-10-28 09:53:24 -0500

    Separate the component functions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,24 @@
 import './ExpenseItem.css';
 
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = "Paper";
-    const exmenseAmount = 294.23
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', { month: "long" });
+    const day = props.date.toLocaleString('en-US', { day: "2-digit" });
+    const year = props.date.getFullYear();
+
     return (
         <div className="expense-item">
-            <div>{expenseDate}</div>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">Am
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
